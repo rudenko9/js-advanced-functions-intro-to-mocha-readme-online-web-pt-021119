e0311b80fa319f8dd82b2ab186f817dc5d4944fe
Nataliya R <n.rudenko@gmail.com>
2019-10-01 00:37:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+const theTruth = function() {
+  return  true;
+}
+
+function  sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor) {
+  return `I love ${flavor}`
+}
+
+function shouting(string) {
+ return string.toUpperCase()
+}
+
+function roundDown(number) {
+  return parseInt(number)
+}
